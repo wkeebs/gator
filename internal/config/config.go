2ae69d8029b4d51f6507ffb7a765071ddc35ab8a
Will Keeble <will.a.keeble@gmail.com>
2024-12-04 15:27:48 +1100

    add SetUser to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,28 +2,60 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 )
+
+const configFileName = "gatorconfig.json"
 
 type Config struct {
 	DbUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read(configFileName string) (Config, error) {
+func getConfigFilePath() (string, error) {
 	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configFilePath := homeDir + "/" + configFileName
+
+	return configFilePath, nil
+}
+
+func write(cfg Config) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	// marshal Config data to write
+	configData, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(configFilePath, configData, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
 	if err != nil {
 		return Config{}, err
 	}
 
-	configFilePath := homeDir + "/" + configFileName
-	fmt.Println(configFilePath)
+	// read from ~/gatorconfig.json
 	configData, err := os.ReadFile(configFilePath)
 	if err != nil {
 		return Config{}, err
 	}
 
+	// unmarshal into Config struct
 	var configStruct Config
 	err = json.Unmarshal(configData, &configStruct)
 	if err != nil {
@@ -31,4 +63,14 @@ func Read(configFileName string) (Config, error) {
 	}
 
 	return configStruct, nil
+}
+
+func (c *Config) SetUser(newUserName string) error {
+	c.CurrentUserName = newUserName
+	err := write(*c)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 	"github.com/wkeebs/rss-blog-aggregator/internal/config"
 )
 
-const CONFIG_FILENAME = "gatorconfig.json"
-
 func main() {
-	configData, err := config.Read(CONFIG_FILENAME)
+	configStruct, err := config.Read()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(configData)
+	fmt.Println(configStruct)
+
+	configStruct.SetUser("Will")
+
+	fmt.Println(configStruct)
+
 }
